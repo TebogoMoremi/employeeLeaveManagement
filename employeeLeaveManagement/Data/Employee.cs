795602a386a0eae49781cdb7c59b6998d8db2b41
Tebogo Herrold Moremi <s219372934@mandela.ac.za>
2022-07-15 01:40:24 +0200

    Add project files.

---
+++ employeeLeaveManagement/Data/Employee.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace employeeLeaveManagement.Data
+{
+    public class Employee: IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string TaxID { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateJoined { get; set; }
+
+
+
+    }
+}
